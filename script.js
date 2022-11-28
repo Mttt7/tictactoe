3e21ddfdc203475935b5090b37540f32ce5f785a
mttt <groobytt@gmail.com>
2022-11-28 19:37:32 +0100

    finish choosing heroes and add displaying info when wrong chosing

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 const startVsPlayerButton = document.querySelector(".vs-player-btn")
-const startVsAiButton = document.querySelector("vs-ai-btn")
-
+const startVsAiButton = document.querySelector(".vs-ai-btn")
 const characters = document.querySelectorAll(".character")
+const info = document.querySelector("#info")
 
-
+// Settings
+info.innerText = ""
+info.classList.add("hidden")
+//
 
 characters.forEach(ch=>{
-    ch.addEventListener('click',MarkChoose)
+    ch.addEventListener('click',markChoose)
 })
 
 
-function MarkChoose(e){
+function markChoose(e){
+    info.innerText = ""
+    info.classList.add("hidden")
     let char = e.target
     let charId
     if(e.target.nodeName == 'DIV'){
@@ -22,18 +27,14 @@ function MarkChoose(e){
         charId = char.parentNode.dataset.characterId
     }
     console.log(charId)
-    Mark(charId)
+    mark(charId)
 
 }
 let selectedArrPl = []
 let selectedArrAi = []
 
-function Mark(charId){
+function mark(charId){
     let selected = document.querySelector(`[data-character-id="${charId}"]`)
-    
-    
-    
-
     if(selectedArrPl.includes(charId) || selectedArrAi.includes(charId)){
        if(charId==0 || charId==1 || charId==2){
         selectedArrPl = selectedArrPl.filter(ch => ch!=charId)
@@ -65,4 +66,48 @@ function Mark(charId){
         selected.children[0].classList.add("selected")
     }
     
+}
+
+
+startVsPlayerButton.addEventListener('click',chooseMode)
+startVsAiButton.addEventListener('click',chooseMode)
+
+function characterChosen(){
+    if (selectedArrPl.length==1 && selectedArrAi.length==1) return 0 //git
+    if (selectedArrPl.length==1 && selectedArrAi.length!=1) return 1 //wybrac evila
+    if (selectedArrPl.length!=1 && selectedArrAi.length==1) return 2 // wybrac playera
+    else return -1 //wybrac wszystko
+}
+
+function chooseMode(e){
+    //player - 0
+    //ai - 1
+    
+    if(characterChosen()==0){
+
+        if(e.target.classList[0]=="vs-player-btn") startGame(0)
+        if(e.target.classList[0]=="vs-ai-btn") return
+    }
+    if(characterChosen()==2){
+        info.classList.remove("hidden")
+        console.log("Wybierz bohatera dla gracza!")
+        info.innerText = "Wybierz bohatera dla gracza!"
+    }
+    if(characterChosen()==1){
+        info.classList.remove("hidden")
+        console.log("wybierz bohatera dla ai!")
+        info.innerText = "Wybierz bohatera dla ai!"
+    }
+    if(characterChosen()==-1){
+        info.classList.remove("hidden")
+        console.log("Wybierz bohaterow!")
+        info.innerText = "Wybierz bohaterow!"
+    }
+    
+}
+
+function startGame(mode){
+    if(mode==0){
+
+    }
 }
